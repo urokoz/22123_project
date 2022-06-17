efa49ab778fa6934af262a125dc0000435c5b4b7
JoachimBreitenstein <joachim.breitenstein@hotmail.com>
2022-06-17 15:37:09 +0200

    func_file update

--- R/02_CIT_signature.R
+++ R/02_CIT_signature.R
@@ -24,182 +24,8 @@ source("R/99_func_file.R")
 
 # Calculate which genes are significantly different for each subtype
 
-significant_genes <- function(expr_data, classes) {
-  
-  signif_genes <- c()
-  unique_classes <- unique(classes)
-  for (class in unique(classes)) {
-    
-    results <- c()
-    for (probe in rownames(expr_data)) {
-      is_class <- expr_data[probe, classes == class]
-      rest <- expr_data[probe, classes != class]
-      res <- wilcox.test(x = as.numeric(is_class),
-                         y = as.numeric(rest))
-      results[[probe]] <- res$p.value
-      
-    }
-    results <- p.adjust(results)
-    results <- results[results < 0.05]
-    
-    signif_genes[[class]] <- results
-    
-  }
-  return(signif_genes)
-}
-
 save(significant_genes(CIT_full, CIT_classes), file = "data/CIT_signif_subtype_genes.Rdata")
 save(significant_genes(GBM_expr, GBM_clinical$GeneExp_Subtype), file = "data/GBM_signif_subtype_genes.Rdata")
-# load("data/CIT_signif_subtype_genes.Rdata")
-
-#for (class in unique(CIT_classes)) {
-  
- # results <- c()
-  #for (probe in rownames(CIT_full)) {
-   # is_class <- CIT_full[probe, CIT_classes == class]
-   # rest <- CIT_full[probe, CIT_classes != class]
-   # res <- wilcox.test(x = as.numeric(is_class),
-    #                   y = as.numeric(rest))
-    #results[[probe]] <- res$p.value
-    
-  #}
-  # results <- p.adjust(results)
-  # results <- results[results < 0.05]
-  
-  # signif_genes[[class]] <- results
-  
-#}
-
-
-## Calculate foldchange signatures
-
-FC_calc <- function(expr_data, classes) {
-  
-  FC_lists <- c()
-  for (class in unique(classes)) {
-    results <- signif_genes[[class]]
-    
-    FC_list <- c()
-    
-    for (probe in names(results)) {
-      subtype_median <- median(as.numeric(expr_data[probe, classes == class]))
-      rest_median <- median(as.numeric(expr_data[probe, classes != class]))
-      
-      FC_list <- rbind(FC_list, c(probe, log2(subtype_median/rest_median)))
-      
-    }
-    colnames(FC_list) <- c("probe", "FC")
-    FC_list <- data.frame(FC_list)
-    signa <- arrange(data.frame(FC_list), desc(FC))
-    
-    file_name <- sprintf("data/ranked_CIT_%s_probes.txt", class)
-    
-    write.table(signa$probe, file = file_name, quote = F, row.names = F, col.names = F)
-    
-    FC_lists[[class]] <- signa
-    
-  }
-  return(FC_lists)
-}
-
-#FC_lists <- c()
-#for (class in unique(CIT_classes)) {
-#  results <- signif_genes[[class]]
-  
-#  FC_list <- c()
-  
-#  for (probe in names(results)) {
-#    subtype_median <- median(as.numeric(CIT_full[probe, CIT_classes == class]))
-#    rest_median <- median(as.numeric(CIT_full[probe, CIT_classes != class]))
-#    
-#    FC_list <- rbind(FC_list, c(probe, log2(subtype_median/rest_median)))
-    
-#  }
-#  colnames(FC_list) <- c("probe", "FC")
-#  FC_list <- data.frame(FC_list)
-#  signa <- arrange(data.frame(FC_list), desc(FC))
-  
-#  file_name <- sprintf("data/ranked_CIT_%s_probes.txt", class)
-  
-#  write.table(signa$probe, file = file_name, quote = F, row.names = F, col.names = F)
-  
-#  FC_lists[[class]] <- signa
-  
-#}
-
-pred_perf <- function(signa, class) {
-  enrich <- gsva(CIT_full,
-                 signa,
-                 method="ssgsea",
-                 ssgsea.norm = F,
-                 verbose=F)
-  return(ks.test(enrich[CIT_classes == class], enrich[CIT_classes != class])$statistic)
-}
-
-
-calc_signatures <- function(FC_lists, classes) {
-  
-  signatures <- c()
-  performances <- c()
-  
-  for (class in unique(classes)) {
-    sig_probes <- FC_lists[[class]]$probe
-    
-    best_perf <- 0
-    conseq_worse <- 0
-    best_size <- NULL
-    for (i in 1:length(sig_probes)) {
-      signa <- list(sig_probes[1:i])
-      
-      perf <- pred_perf(signa, class)
-      print(sprintf("%s, %d:  %f", class, i, perf))
-      performances <- rbind(performances, t(c(class, i, perf)))
-      
-      if (perf <= best_perf) {
-        conseq_worse <- conseq_worse + 1
-        if (conseq_worse == 25) {
-          break
-        }
-      } else {
-        conseq_worse <- 0
-        best_perf <- perf
-        best_size <- i
-      }
-    }
-    signatures[[class]] <- list(sig_probes[1:best_size])
-  }
-  return(signatures)
-}
-
-#signatures <- c()
-#performances <- c()
-
-#for (class in c("lumB", "lumC", "basL")) {
-#  sig_probes <- FC_lists[[class]]$probe
-  
-#  best_perf <- 0
-#  conseq_worse <- 0
-#  best_size <- NULL
-#  for (i in 1:length(sig_probes)) {
-#    signa <- list(sig_probes[1:i])
-    
-#    perf <- pred_perf(signa, class)
-#    print(sprintf("%s, %d:  %f", class, i, perf))
-#    performances <- rbind(performances, t(c(class, i, perf)))
-    
-#    if (perf <= best_perf) {
-#      conseq_worse <- conseq_worse + 1
-#      if (conseq_worse == 25) {
-#        break
-#      }
-#    } else {
-#      conseq_worse <- 0
-#      best_perf <- perf
-#      best_size <- i
-#    }
-#  }
-#  signatures[[class]] <- list(sig_probes[1:best_size])
-#}
 
 save(signatures, file = "data/signatures.Rdata")
 load("data/signatures.Rdata")
@@ -209,15 +35,15 @@ for (class in unique(CIT_classes)) {
   colnames(signa) <- c("Probe.Set.ID")
   
   df_joined <- Bordet_annot %>%
-  select(Probe.Set.ID, Gene.Symbol) %>% 
-  mutate(Gene.Symbol = str_extract(Gene.Symbol, "^\\S+")) %>%
-  inner_join(signa, by = "Probe.Set.ID") %>% 
-  filter(Gene.Symbol != "---")
+    select(Probe.Set.ID, Gene.Symbol) %>% 
+    mutate(Gene.Symbol = str_extract(Gene.Symbol, "^\\S+")) %>%
+    inner_join(signa, by = "Probe.Set.ID") %>% 
+    filter(Gene.Symbol != "---")
   
   file_name <- sprintf("data/signature_CIT_%s_genes.txt", class)
   write.table(df_joined$Gene.Symbol, file = file_name, quote = F, row.names = F, col.names = F)
 }
-  
+
 df <- data.frame(performances)
 colnames(df) <- c("Subtype", "Sig_len", "Perf")
 df$Perf <- as.numeric(as.character(df$Perf))
@@ -225,8 +51,8 @@ df$Sig_len <- as.numeric(as.character(df$Sig_len))
 
 df %>% 
   ggplot(aes(x = Sig_len,
-                 y = Perf,
-                 color = Subtype)) +
+             y = Perf,
+             color = Subtype)) +
   geom_line() + 
   labs(title = "Performance of variation in subtypes' signature length", x = "Number of genes in signatures", y = "Performance")
 
@@ -252,5 +78,4 @@ rownames(enrich) <- c("normL", "lumA", "lumB", "mApo", "basL", "lumC")
 pred_class <- apply(enrich, 2, name_max)
 
 mean(CIT_classes == pred_class)
-
 
--- R/99_func_file.R
+++ R/99_func_file.R
@@ -82,14 +82,6 @@ probe_to_gene <- function(data, method) {
   return(final_table[2:nrow(final_table),])
 }
 
-#test
-#Brodet_df_joined %>% select(Gene.Symbol, HER2.13:ncol(Brodet_df_joined)) %>% 
- # pivot_longer(cols = !Gene.Symbol, names_to = "Sample", values_to = "Expression") %>% 
-  #group_by(Gene.Symbol, Sample) %>% 
-  #summarise(Median = median(Expression)) %>% 
-  #pivot_wider(names_from = Sample, values_from = Median)
-
-
 
 signature_procsess <- function(df, specification, method) {
   if (specification == "CIT") {
@@ -138,6 +130,129 @@ boxplot <- function(df, variable_dis, variable_con, title, xlab, ylab) {
   
   return(plt)
 }
+
+probeID_to_geneID <- function(df) {
+  
+  colnames(df) <- c("Probe.Set.ID", "Expression") 
+  geneID <- df %>% left_join(CIT_annot, by = "Probe.Set.ID")
+  
+  return(geneID)
+}
+
+df <- load()
+read.delim("data/top_or_bottom_25/normL_rest.txt")
+probeID_to_geneID()
+
+significant_genes <- function(expr_data, classes) {
+  
+  signif_genes <- c()
+  unique_classes <- unique(classes)
+  for (class in unique(classes)) {
+    
+    results <- c()
+    for (probe in rownames(expr_data)) {
+      is_class <- expr_data[probe, classes == class]
+      rest <- expr_data[probe, classes != class]
+      res <- wilcox.test(x = as.numeric(is_class),
+                         y = as.numeric(rest))
+      results[[probe]] <- res$p.value
+      
+    }
+    results <- p.adjust(results)
+    results <- results[results < 0.05]
+    
+    signif_genes[[class]] <- results
+    
+  }
+  return(signif_genes)
+}
+
+save(significant_genes(CIT_full, CIT_classes), file = "data/CIT_signif_subtype_genes.Rdata")
+save(significant_genes(GBM_expr, GBM_clinical$GeneExp_Subtype), file = "data/GBM_signif_subtype_genes.Rdata")
+
+
+FC_calc <- function(expr_data, classes) {
+  
+  FC_lists <- c()
+  for (class in unique(classes)) {
+    results <- signif_genes[[class]]
+    
+    FC_list <- c()
+    
+    for (probe in names(results)) {
+      subtype_median <- median(as.numeric(expr_data[probe, classes == class]))
+      rest_median <- median(as.numeric(expr_data[probe, classes != class]))
+      
+      FC_list <- rbind(FC_list, c(probe, log2(subtype_median/rest_median)))
+      
+    }
+    colnames(FC_list) <- c("probe", "FC")
+    FC_list <- data.frame(FC_list)
+    signa <- arrange(data.frame(FC_list), desc(FC))
+    
+    file_name <- sprintf("data/ranked_CIT_%s_probes.txt", class)
+    
+    write.table(signa$probe, file = file_name, quote = F, row.names = F, col.names = F)
+    
+    FC_lists[[class]] <- signa
+    
+  }
+  return(FC_lists)
+}
+
+
+
+pred_perf <- function(signa, class) {
+  enrich <- gsva(CIT_full,
+                 signa,
+                 method="ssgsea",
+                 ssgsea.norm = F,
+                 verbose=F)
+  return(ks.test(enrich[CIT_classes == class], enrich[CIT_classes != class])$statistic)
+}
+
+
+calc_signatures <- function(FC_lists, classes) {
+  
+  signatures <- c()
+  performances <- c()
+  
+  for (class in unique(classes)) {
+    sig_probes <- FC_lists[[class]]$probe
+    
+    best_perf <- 0
+    conseq_worse <- 0
+    best_size <- NULL
+    for (i in 1:length(sig_probes)) {
+      signa <- list(sig_probes[1:i])
+      
+      perf <- pred_perf(signa, class)
+      print(sprintf("%s, %d:  %f", class, i, perf))
+      performances <- rbind(performances, t(c(class, i, perf)))
+      
+      if (perf <= best_perf) {
+        conseq_worse <- conseq_worse + 1
+        if (conseq_worse == 25) {
+          break
+        }
+      } else {
+        conseq_worse <- 0
+        best_perf <- perf
+        best_size <- i
+      }
+    }
+    signatures[[class]] <- list(sig_probes[1:best_size])
+  }
+  return(signatures)
+}
+
+
+# For each sample, the signature with the highest enrichment corresponds to the subtype you assign to the given sample
+name_max <- function(column) {
+  subtype <- names(column)[which.max(column)]
+  return(subtype)
+}
+
 
 
 
